8db0a4892a1a7bb8f0973e0837606b1a13d28922
chenjiarong <chenjiarong@xindong.com>
2020-05-27 16:38:14 +0800

    change grpc example

--- example/grpc.go
+++ example/grpc.go
@@ -8,18 +8,22 @@ import (
 )
 
 func main() {
-	proxy, err := middleware.NewGRPCProxyMiddleware("localhost:20002",
-		[]string{"./example/service.protoset"}, grpc.WithInsecure())
+	// grpc service IP地址
+	address := "localhost:8080"
+	// grpc协议描述文件，参考：https://github.com/fullstorydev/grpcurl#protoset-files
+	descriptor := []string{"./example/service.protoset"}
+	proxy, err := middleware.NewGRPCProxyMiddleware(address, descriptor, grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
 
 	reg, err := register.New(&register.Config{
-		App:     "test", // 请修改配置参数
-		Name:    "test",
-		Key:     "test",
+		App:     "test", // 请修改对应的App缩写
+		Name:    "test", // 请填入服务名，若协议Package为xdapp.api.v1则填入xdapp即可
+		Key:     "test", // 从服务管理中添加服务后获取
 		IsDebug: false,
 	})
+
 	if err != nil {
 		panic(err)
 	}
